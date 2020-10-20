f40e7cb129fdca2d2465cd898579b436c318ab90
stoqel33 <stochel202@interia.pl>
2020-10-20 09:10:52 +0200

    handle 404 page, add style

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import { Link } from 'gatsby';
+import MainLayout from '../components/mainLayout/mainLayout';
+import style from './404.module.scss';
 
-const NotFoundPage = () => <></>;
+const NotFoundPage = () => (
+  <MainLayout>
+    <div className={style.page}>
+      <p className={style.text}>Page does't exists</p>
+      <Link to="/" className={style.link}>
+        To Start Page
+      </Link>
+    </div>
+  </MainLayout>
+);
 
 export default NotFoundPage;
